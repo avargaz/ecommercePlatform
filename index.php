ec572a63f4b3941205a6f64647927c40008bedcd
avargaz <alexivamo@hotmail.com>
2017-03-12 20:05:41 -0600

    layout config

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE>
+<html>
+    <head>
+        <title> My Online Store</title>
+        <link rel="stylesheet" href="css/style.css" media="all"/>
+    </head>
+    
+<body>
+
+    <div class="main_wrapper">
+        Container
+        
+        <div class="header_wrapper">
+            <img id="logo" src="img/logo.jpg"/>
+            <img id="banner" src="img/logo.jpg"/>
+            <div class="menubar"> This is menu bar</div>
+        </div>
+        <div class="content_wrapper"> 
+            
+            <div id="sidebar">This is the sidebar!</div>
+            <div id="content_area">This is the content area!</div>
+        </div>
+        <div id="footer">This is the footer!</div>
+    </div>
+
+</body>
+
+
+
+</html>
