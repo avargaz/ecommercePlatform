1dd6db6ec082e1266caed7299d025c3eb2ff16f2
avargaz <alexivamo@hotmail.com>
2017-03-13 08:41:21 -0600

    menu Bar
    added menu bar with dead links temporarily

--- index.php
+++ index.php
@@ -8,19 +8,47 @@
 <body>
 
     <div class="main_wrapper">
-        Container
         
+        <!--Header starts here-->
         <div class="header_wrapper">
             <img id="logo" src="img/logo.jpg"/>
             <img id="banner" src="img/logo.jpg"/>
-            <div class="menubar"> This is menu bar</div>
         </div>
+        <!--Header ends here-->
+        <!--Menubar starts here-->
+        <div class="menubar">
+            <ul id="menu">
+                <li><a href="#">Home</a> </li>
+                <li><a href="#">Products</a> </li>
+                <li><a href="#">My Account</a> </li>
+                <li><a href="#">Sign Up</a> </li>
+                <li><a href="#">Shopping Cart</a> </li>
+                <li><a href="#">Contact Us</a> </li>
+                <div id="form">
+                    <form method="get" action="results.php" enctype="multipart/form-data">
+                        <input type="text" name="user_query" placeholder="Search a Product" />
+                        <input type="submit" name="search" value="Search"/>
+                    </form>
+                </div>
+            </ul>
+            
+
+        </div>
+        <!--Menubar ends here-->
+        <!--Content starts here-->
         <div class="content_wrapper"> 
             
-            <div id="sidebar">This is the sidebar!</div>
+            <div id="sidebar">
+                
+                <div id="sidebar_title">Categories </div>
+            
+            </div>
             <div id="content_area">This is the content area!</div>
         </div>
+        <!--Content ends here-->
+        <!--Footer starts here-->
         <div id="footer">This is the footer!</div>
+        <!--Footer ends here-->
     </div>
 
 </body>
